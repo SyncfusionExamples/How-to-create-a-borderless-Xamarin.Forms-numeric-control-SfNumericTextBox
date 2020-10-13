5a8fdd35f7173df46572d5aace1ed213482aacf1
AzureAD\SrideviSivakumar <AzureAD\sridevi.sivakumar@syncfusion.com>
2020-10-13 12:21:17 +0530

    XAMARIN-37374_BorderlessNumericTextBox sample committed

---
+++ Borderless_TextBox/Borderless_TextBox.Android/Renderer/CustomRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Android.Graphics.Drawables;
+using Borderless_TextBox;
+using Borderless_TextBox.Droid;
+using Syncfusion.SfNumericTextBox.XForms.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomNumericTextBox), typeof(CustomNumericTextBoxRenderer))]
+namespace Borderless_TextBox.Droid
+{
+    public class CustomNumericTextBoxRenderer : SfNumericTextBoxRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Syncfusion.SfNumericTextBox.XForms.SfNumericTextBox> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.Background = null;
+            }
+        }
+    }
+}
---
+++ Borderless_TextBox/Borderless_TextBox.iOS/Renderer/CustomRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Borderless_TextBox;
+using Borderless_TextBox.iOS;
+using Syncfusion.SfNumericTextBox.XForms;
+using Syncfusion.SfNumericTextBox.XForms.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomNumericTextBox), typeof(CustomNumericTextBoxRenderer))]
+namespace Borderless_TextBox.iOS
+{
+    class CustomNumericTextBoxRenderer : SfNumericTextBoxRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<SfNumericTextBox> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.BorderStyle = UIKit.UITextBorderStyle.None;
+                Control.Layer.CornerRadius = 0f;
+                Control.Layer.BorderColor = Color.Transparent.ToCGColor();
+                Control.Layer.BorderWidth = 0;
+            }
+        }
+    }
+}
